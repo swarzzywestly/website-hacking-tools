74c5f16b0a90c8ed44878e775a864313f9970278
Chukwuji Obinna <82438612+swarzzywestly@users.noreply.github.com>
2023-10-28 08:06:11 +0100

    Add files via upload

---
+++ crawler.py
@@ -0,0 +1,12 @@
+#!/user/bin/env python
+
+# sending a GET Requests to web servers.
+
+import requests
+
+url = "gfdsfssfsf.google.com"
+try:
+    get_response = requests.get("http://" + url)
+    print(get_response)
+except requests.exceptions.ConnectionError:
+    pass
