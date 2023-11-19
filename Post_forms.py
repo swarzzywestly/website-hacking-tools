816ff525bc4a6ae62e2abaa815ef84bd2efbb555
Chukwuji Obinna <82438612+swarzzywestly@users.noreply.github.com>
2023-11-19 17:44:01 +0100

    Delete Post_forms.py

--- Post_forms.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# using python to send a post request to websites.
-
-import requests
-
-target_url = "http://192.168.110.155/dvwa/login.php"
-data_dict = {"username": "admin", "password": "password", "Login": "submit"}
-response = requests.post(target_url, data=data_dict)
-print(response.content)
